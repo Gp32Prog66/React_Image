e1474237287c9fda036873d613fe07a1c29eeb75
Gp32Prog66 <szojo7@hotmail.com>
2024-04-01 15:27:31 -0400

    Adding

---
+++ imagegames/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import imagePitt from './images/downtown.jpg';
+
+
+function Image()
+{
+  return (
+    <div>
+      <img src={imagePitt} alt = ""/>
+    </div>
+  )
+}
+
+export default Image
